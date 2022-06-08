72c857ccb04c7c9028c73e56ecba44b81365c0a6
nandainacio <fp_inacio@hotmail.com>
2022-06-08 08:27:16 -0300

    Exercício sobre Matriz

---
+++ Matrizes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ TRABALHANDO COM MATRIZ ------");
+            Console.WriteLine("Digite um número (1 a 10)");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Sua matriz terá {num} linhas e {num} colunas.");
+
+            double [,] mat = new double [num,num];
+
+            for(int i = 0; i < num; i++)
+            {
+                string[] vetores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < num; j++)
+                {
+                    mat[i, j] = double.Parse(vetores[j]);
+                }
+            }
+
+            Console.WriteLine("--- MATRIZ DIAGONAL ---");
+            for(int i = 0; i < num; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < num; i++)
+            {
+
+                for (int j = 0; j < num; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Números negativos = {count}");
+        }
+    }
+}
